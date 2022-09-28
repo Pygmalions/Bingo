1652557f5d46ab7d122653e8dbe3752bbbe69a0b
Vincent Jia <rumble.vecent@outlook.com>
2022-09-28 23:19:42 +0800

    Add RepeatAny.cs to support 0 or more times repetition.

---
+++ Bingo.1D/Patterns/RepeatAny.cs
@@ -0,0 +1,24 @@
+﻿namespace Bingo.One.Patterns;
+
+/// <summary>
+/// Try to match the condition for any times (includes 0),
+/// which means that this pattern will always match.
+/// </summary>
+public class RepeatAny<TElement> : Pattern<TElement>
+{
+    public readonly Pattern<TElement> Condition;
+
+    public RepeatAny(Pattern<TElement> condition)
+    {
+        Condition = condition;
+    }
+
+    public override bool Match(Context<TElement> context)
+    {
+        while (Condition.Match(context))
+        {}
+        return true;
+    }
+
+    public override bool Equivalent(Pattern<TElement> target) => target is RepeatAny<TElement>;
+}
